ce68ead731cb70af4a23ed18738a4a7ecc8ed3c7
Kevin Rivera <kevinpr_007@hotmail.com>
2017-02-23 21:53:09 -0400

    starting add reducer

--- crud/src/index.js
+++ crud/src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { createStore } from 'redux'
+import rootReducer from './rootReducer'
+
+const store = createStore(
+  rootReducer
+)
 
 ReactDOM.render(
   <App />,
