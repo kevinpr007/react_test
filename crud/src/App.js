096066bf75faadedfef0272aea2fa369449166f4
Kevin Rivera <kevinpr_007@hotmail.com>
2017-03-01 20:47:11 -0400

    Complete video 4

--- crud/src/App.js
+++ crud/src/App.js
@@ -3,21 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import { Link, Route } from 'react-router-dom'
 import GamesPage from './GamesPage'
+import GameForm from './GameForm'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="ui container">
+        <div className="ui three item menu">
+          {/*<div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            <Link to="games">games</Link>
+          </p>*/}
+
+
+          <Link className="item" to="/">Home</Link>
+          <Link className="item" to="/games">Games</Link>
+          <Link className="item" to="/games/new">Add New Game</Link>
+
         </div>
-        <p className="App-intro">
-          <Link to="games">games</Link>
-        </p>
-
         <Route exact path="/games" component={GamesPage} />
-
+        <Route exact path="/games/new" component={GameForm} />
       </div>
     );
   }
